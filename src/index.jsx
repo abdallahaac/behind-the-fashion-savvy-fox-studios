b688a99e6856d778a88f9ba7a962ccc61fd2f5c7
abdallahac <aceydesignz@gmail.com>
2024-11-29 14:56:48 -0500

    added changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,33 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
-import * as THREE from 'three'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import * as THREE from "three";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <Canvas
-        gl={ {
-            antialias: true,
-            toneMapping: THREE.ACESFilmicToneMapping,
-            // outputColorSpace: THREE.SRGBColorSpace
-        } }
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ 3, 2, 6 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
-)
+	<div className="app">
+		<div className="canvas-container">
+			<Canvas
+				gl={{
+					antialias: true,
+					toneMapping: THREE.ACESFilmicToneMapping,
+				}}
+				camera={{
+					fov: 45,
+					near: 0.1,
+					far: 200,
+					position: [3, 2, 6],
+				}}
+			>
+				<Experience />
+			</Canvas>
+		</div>
+		<div className="ui-container">
+			<h1>React 2D UI</h1>
+			<p>Add your controls or content here.</p>
+			<button>Click Me</button>
+		</div>
+	</div>
+);
