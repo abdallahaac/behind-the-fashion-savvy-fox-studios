6bb6c20042c46f026ba448616bbb53e564daef78
abdallahac <aceydesignz@gmail.com>
2025-02-28 04:15:08 -0500

    merged files

--- src/utils/QuizQuestions.jsx
+++ src/utils/QuizQuestions.jsx
@@ -1,29 +1,41 @@
 const QuizQuestions = [
-    {
-        question: "What is the SA8000 Standard Certification that factories can obtain?",
-        answers: ["It shows the factory’s commitment to use clean energy and reduce waste", "It reflects the factory's standards for treating workers fairly"],
-        correctAnswer: "It reflects the factory's standards for treating workers fairly",
-        reasoning:"SA8000 is a standard that organizations meet to show their commitment to treating workers fairly. ",
-        winAmount: 30000,
-        loseAmount: -30000,
-    },
-    {
-        question: "What is a disadvantage of using cotton as a fabric material? ",
-        answers: ["It is an expensive material", "It releases microplastics", "It releases toxins into the environment", "It uses a lot of water"],
-        correctAnswer: "It is an expensive material",
-        reasoning:"Conventional cotton uses up to 25% of all the pesticides used in farming",
-        winAmount: 10000,
-        loseAmount: -10000,
-    },
-    {
-        question: "What is the largest planet in our solar system?",
-        answers: ["Earth", "Mars", "Jupiter", "Saturn"],
-        correctAnswer: "Jupiter",
-        reasoning:"Conventional cotton uses up to 25% of all the pesticides used in farming",
-        winAmount: 1500,
-        loseAmount: -700,
-    },
-    // Add more questions as needed
+	{
+		question:
+			"What is the SA8000 Standard Certification that factories can obtain?",
+		answers: [
+			"It shows the factory’s commitment to use clean energy and reduce waste",
+			"It reflects the factory's standards for treating workers fairly",
+		],
+		correctAnswer:
+			"It reflects the factory's standards for treating workers fairly",
+		reasoning:
+			"SA8000 is a standard that organizations meet to show their commitment to treating workers fairly. ",
+		winAmount: 30000,
+		loseAmount: -30000,
+	},
+	{
+		question: "What is a disadvantage of using cotton as a fabric material? ",
+		answers: [
+			"It is an expensive material",
+			"It releases microplastics",
+			"It releases toxins into the environment",
+			"It uses a lot of water",
+		],
+		correctAnswer: "It is an expensive material",
+		reasoning:
+			"Conventional cotton uses up to 25% of all the pesticides used in farming",
+		winAmount: 10000,
+		loseAmount: -10000,
+	},
+	// {
+	//     question: "What is the largest planet in our solar system?",
+	//     answers: ["Earth", "Mars", "Jupiter", "Saturn"],
+	//     correctAnswer: "Jupiter",
+	//     reasoning:"Conventional cotton uses up to 25% of all the pesticides used in farming",
+	//     winAmount: 1500,
+	//     loseAmount: -700,
+	// },
+	// Add more questions as needed
 ];
 
 export default QuizQuestions;
