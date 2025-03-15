ed38af426658726bbb4da2b3b89fb42dd6af986b
abdallahac <aceydesignz@gmail.com>
2025-03-15 17:43:00 -0400

    added new branch

--- src/components/FullScreenVideo.jsx
+++ src/components/FullScreenVideo.jsx
@@ -1,26 +1,33 @@
-import React, { useRef, useEffect } from 'react';
-import "../assets/styles/full-screen-video.css"; 
+import React, { useRef, useEffect } from "react";
+import "../assets/styles/full-screen-video.css";
 
 const FullScreenVideo = ({ videoSrc, onVideoEnd }) => {
-    const videoRef = useRef(null);
+	const videoRef = useRef(null);
 
-    useEffect(() => {
-        const videoElement = videoRef.current;
-        if (videoElement) {
-            videoElement.addEventListener('ended', onVideoEnd);
-        }
-        return () => {
-            if (videoElement) {
-                videoElement.removeEventListener('ended', onVideoEnd);
-            }
-        };
-    }, [onVideoEnd]);
+	useEffect(() => {
+		const videoElement = videoRef.current;
+		if (videoElement) {
+			videoElement.addEventListener("ended", onVideoEnd);
+		}
+		return () => {
+			if (videoElement) {
+				videoElement.removeEventListener("ended", onVideoEnd);
+			}
+		};
+	}, [onVideoEnd]);
 
-    return (
-        <div className="video-container">
-            <video className="full-screen-video" ref={videoRef} src={videoSrc} autoPlay controls />
-        </div>
-    );
+	return (
+		<div className="video-container">
+			<video
+				className="full-screen-video"
+				ref={videoRef}
+				src={videoSrc}
+				autoPlay
+				// controls={false}
+				controls
+			/>
+		</div>
+	);
 };
 
 export default FullScreenVideo;
